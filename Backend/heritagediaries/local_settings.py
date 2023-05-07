56bc7e8407b107cfcd4b6aca3f760d9a31366acf
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-05-06 20:37:30 -0400

    Project setup

---
+++ Backend/heritagediaries/local_settings.py
@@ -0,0 +1,12 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-2e-e!_pnj_98@vli9*$dmoiwh8!2el1k!4ux06_e45!ux*0328'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'heritage_diaries_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'My$&1@1993'
+    }
+}
