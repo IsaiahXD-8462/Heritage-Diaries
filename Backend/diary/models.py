397e2dcd5bd407921b9f4bc49aa05c724a16d02d
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-05-06 23:47:32 -0400

    Add diary app

---
+++ Backend/diary/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from authentication.models import User
+
+class Diary(models.Model):
+    picture = models.ImageField()
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    hair_color = models.CharField(max_length=255)
+    eye_color = models.CharField(max_length=255)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    medical_conditions = models.ForeignKey(User, on_delete=models.CASCADE)
+    life_story = models.ForeignKey(User, on_delete=models.CASCADE)
+    DNA_profile = models.ForeignKey(User, on_delete=models.CASCADE)
