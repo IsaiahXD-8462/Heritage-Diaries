d33400a122cdc7fa340b4bc15c05954060f83844
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-05-07 02:37:05 -0400

    added serializers and urls files and updated models

--- Backend/medical/models.py
+++ Backend/medical/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+class Diary(models.Model):
+    Diagnosis = models.CharField(max_length=255)
--- Backend/story/models.py
+++ Backend/story/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+class Diary(models.Model):
+    place_of_birth = models.CharField(max_length=255)
+    education = models.CharField(max_length=255)
+    number_of_siblings = models.IntegerField()
+    number_of_children = models.IntegerField()
+    number_of_grandchildren = models.IntegerField()
