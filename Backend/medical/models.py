21dd9156be1d1e2ebed62ecd8a1f40b72f7824cc
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-05-07 20:51:49 -0400

    Admin access, model and serializers created fo Medical

--- Backend/medical/models.py
+++ Backend/medical/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 from authentication.models import User
 
-class Diary(models.Model):
-    Diagnosis = models.CharField(max_length=255)
+class Medical(models.Model):
+    Diagnosis = models.TextField(max_length=999)
